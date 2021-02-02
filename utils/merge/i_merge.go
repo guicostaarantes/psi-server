142a6382358ba465633255286b7e48c77f754d0f
guicostaarantes <arantes.g@outlook.com>
2021-02-02 17:21:46 -0300

    📝 documenting IMergeUtil

--- utils/merge/i_merge.go
+++ utils/merge/i_merge.go
@@ -1,5 +1,6 @@
 package merge
 
+// IMergeUtil is an abstraction for a utility that parses the keys of a source interface to a destination interface
 type IMergeUtil interface {
 	Merge(destination interface{}, source interface{}) error
 }
