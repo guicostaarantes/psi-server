825bf6320560e19d58a7f2db8a920b7ea518b7be
guicostaarantes <arantes.g@outlook.com>
2021-02-15 07:40:40 -0300

    ✅ removed generated file from coverage

--- test-e2e.sh
+++ test-e2e.sh
@@ -1,3 +1,3 @@
-go test -failfast -coverpkg ./... -coverprofile ./e2e/cover.out ./e2e
+go test -failfast -coverpkg ./graph/resolvers/...,./modules/... -coverprofile ./e2e/cover.out ./e2e
 go tool cover -html ./e2e/cover.out -o ./e2e/cover.html
 rm -rf ./e2e/cover.out
