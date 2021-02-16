293691ddd945aaaafe2aa08fad8095f161fdbd94
guicostaarantes <arantes.g@outlook.com>
2021-02-16 06:26:23 -0300

    ✅ removed coverage.html from git

--- test-e2e.sh
+++ test-e2e.sh
@@ -1,3 +1,2 @@
 go test -failfast -coverpkg ./graph/resolvers/...,./modules/... -coverprofile ./e2e/cover.out ./e2e
-go tool cover -html ./e2e/cover.out -o ./e2e/cover.html
-rm -rf ./e2e/cover.out
+go tool cover -html ./e2e/cover.out -o ./e2e/coverage.html
