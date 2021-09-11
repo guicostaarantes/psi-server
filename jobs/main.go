3f680e30f7e7de5ab87259e0fe41f1705b886f5e
Guilherme Arantes <arantes.g@outlook.com>
2021-09-11 06:08:26 -0300

    ✨ job frequencies via environment variables

--- jobs/main.go
+++ jobs/main.go
@@ -14,16 +14,20 @@ func print(str string) {
 }
 
 func main() {
+	jobrunnerToken := ""
 	url := os.Getenv("PSI_BACKEND_URL")
 	jobrunnerUser := os.Getenv("PSI_JOBRUNNER_USERNAME")
 	jobrunnerPass := os.Getenv("PSI_JOBRUNNER_PASSWORD")
-	jobrunnerToken := ""
+	getNewTokenFrequency := os.Getenv("PSI_GET_NEW_TOKEN_FREQUENCY")
+	processPendingMailFrequency := os.Getenv("PSI_PROCESS_PENDING_MAIL_FREQUENCY")
+	createPendingAppointmentsFrequency := os.Getenv("PSI_CREATE_PENDING_APPOINTMENTS_FREQUENCY")
 
 	s := gocron.NewScheduler(time.UTC)
+	phase := time.Date(2000, time.January, 1, 12, 0, 0, 0, time.UTC)
 
-	s.Every(10).Seconds().SingletonMode().Do(tasks.GetNewTokenIfNecessary, &jobrunnerToken, url, jobrunnerUser, jobrunnerPass)
-	s.Every(10).Seconds().SingletonMode().Do(tasks.ProcessPendingMail, &jobrunnerToken, url)
-	s.Every(1).Day().At("12:00").SingletonMode().Do(tasks.CreatePendingAppointments, &jobrunnerToken, url)
+	s.Every(getNewTokenFrequency).StartAt(phase).SingletonMode().Do(tasks.GetNewTokenIfNecessary, &jobrunnerToken, url, jobrunnerUser, jobrunnerPass)
+	s.Every(processPendingMailFrequency).StartAt(phase).SingletonMode().Do(tasks.ProcessPendingMail, &jobrunnerToken, url)
+	s.Every(createPendingAppointmentsFrequency).StartAt(phase).SingletonMode().Do(tasks.CreatePendingAppointments, &jobrunnerToken, url)
 
 	s.StartBlocking()
 }
