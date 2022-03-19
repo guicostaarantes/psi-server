377f11c8bb3991faea9d381a8cccfd2b9d37883f
Guilherme Arantes <arantes.g@outlook.com>
2022-03-18 23:17:19 -0300

    🐛 removing unused first name from email template h2

--- modules/users/templates/invite_psychologist_email.go
+++ modules/users/templates/invite_psychologist_email.go
@@ -1,7 +1,7 @@
-package templates
+package users_templates
 
 // InvitePsychologistEmailTemplate is an email template used when a user is invited to join the platform as a psychologist
-var InvitePsychologistEmailTemplate = `<h2>Oi {{ .FirstName }}</h2>
+var InvitePsychologistEmailTemplate = `<h2>Olá 😊</h2>
 <p>Você foi convidado a ingressar no PSI como psicólogo.</p>
 <p>Caso queira aceitar o convite, clique no botão abaixo e crie uma senha pra você acessar nosso site.</p>
 <a href="{{ .SiteURL }}/cadastro?token={{ .Token }}">Criar uma senha</a>`
--- modules/users/templates/reset_password_email.go
+++ modules/users/templates/reset_password_email.go
@@ -1,7 +1,7 @@
-package templates
+package users_templates
 
 // ResetPasswordEmailTemplate is an email template used when a user wants to reset their password
-var ResetPasswordEmailTemplate = `<h2>Oi {{ .FirstName }}</h2>
+var ResetPasswordEmailTemplate = `<h2>Olá 😊</h2>
 <p>Você recebeu esse email pois pediu uma mudança de senha no PSI.</p>
 <p>Para confirmarmos que este email é realmente seu, clique no botão abaixo e crie uma nova senha pra você acessar nosso site.</p>
 <a href="{{ .SiteURL }}/nova-senha?token={{ .Token }}">Mudar minha senha</a>`
